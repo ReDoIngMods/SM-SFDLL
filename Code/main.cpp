10753ca8d02f3377886070f3919ddaad0dadb11c
QuestionableM <77170113+QuestionableM@users.noreply.github.com>
2024-03-13 12:15:23 -0700

    Project files

---
+++ Code/main.cpp
@@ -0,0 +1,23 @@
+#include "Utils/Memory.hpp"
+
+void process_attach()
+{
+	Memory::WriteMemory<std::uint16_t>(0x462F16, 0);
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		process_attach();
+		break;
+	case DLL_PROCESS_DETACH:
+		FreeLibrary(hModule);
+		break;
+	}
+
+	return TRUE;
+}
